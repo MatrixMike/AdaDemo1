7927e45be5edacabffe35449ac518d21241a056e
MikeH <matroxmike@icloud.com>
2018-02-05 12:10:34 +1100

    early commit

---
+++ hello.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+--Print a message out to the screen
+-- date Saturday 13 April 2013
+
+procedure Hello is
+
+begin
+   Put_Line("Hello Mike from Ada");
+   Put_Line("second line");
+end hello;
+
+
