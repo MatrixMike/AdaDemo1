2afa4650159bdc684e5d1f2296a84f1039dc3990
Mike Dell desktop <matroxmike@icloud.com>
2018-02-05 20:47:15 +1100

    more trials

--- square_root.adb
+++ square_root.adb
@@ -1,5 +1,6 @@
 with Ada.Text_IO;
 use Ada.Text_IO;
+use Ada.Numerics.Elementary_Functions;
 
 --procedure call_recur is
 --function fac(n: Natural) is
@@ -13,7 +14,7 @@ use Ada.Text_IO;
 --end fac;
 
 function Square_Root (X : in Float) return Float is
-     use Ada.Numerics.Elementary_Functions;
+use Ada.Numerics.Elementary_Functions;
 begin
      if X < 0.0 then
         return -1.0;
