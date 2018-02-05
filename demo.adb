f40fc1eacb744cd530004ded1d61c393e49fa64d
Mike Dell desktop <matroxmike@icloud.com>
2018-02-05 20:10:31 +1100

    stumbling around2

--- demo.adb
+++ demo.adb
@@ -2,17 +2,17 @@ with Ada.Text_IO;
 use Ada.Text_IO;
 
 procedure demo is
-procedure printlines(numLines, numChars: Natural; c: char) is
+procedure printlines(numLines, numChars: Natural) is
 begin
    for i in 1 .. numLines loop
    for j in 1 .. numChars loop
-       put(c);
+       put('x');
    end loop;
    new_line;
    end loop;
    end printlines;
 begin
-    printlines(2, 20, '-');
+    printlines(2, 20);
    -- do something 
-    printlines(3, 20, '*');
+    printlines(3, 20);
 end demo;
