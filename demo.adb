95f0929c3abf39deeeff8e0b5cb4031d0e801b63
Mike Dell desktop <matroxmike@icloud.com>
2018-02-05 20:19:41 +1100

    stumbling around4

--- demo.adb
+++ demo.adb
@@ -1,19 +1,21 @@
 with Ada.Text_IO;
 use Ada.Text_IO;
+--05.02.2018 20:19:12
+--http://www.radford.edu/nokie/classes/320/Tour/procs.funcs.html
 
 procedure demo is
 procedure printlines(numLines, numChars: Natural; c: Character) is
 begin
    for i in 1 .. numLines loop
    for j in 1 .. numChars loop
-       put('x');
+--       put('x');
        put(c);
    end loop;
    new_line;
    end loop;
    end printlines;
 begin
-    printlines(2, 20, 'x');
+    printlines(2, 20, '=');
    -- do something 
     printlines(3, 20, '-');
 end demo;
