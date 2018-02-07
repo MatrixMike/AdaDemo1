055c62f45b887e411a555ec4942046947e44cf9c
Mike J Hewitt <mikeh@electroteach.com>
2018-02-07 14:49:25 +1100

    tuning and investigating java

--- demo.adb
+++ demo.adb
@@ -1,31 +1,30 @@
-with Ada.Text_IO;
-use Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
 --05.02.2018 20:19:12
 --http://www.radford.edu/nokie/classes/320/Tour/procs.funcs.html
 
 procedure demo is
 
-procedure printlines(numLines, numChars: Natural; c: Character) is
-begin
-   for i in 1 .. numLines loop
-   for j in 1 .. numChars loop
+   procedure printlines (numLines, numChars : Natural; c : Character) is
+   begin
+      for i in 1 .. numLines loop
+         for j in 1 .. numChars loop
 --       put('x');
-       put(c);
-   end loop;
-   new_line;
-   end loop;
+            Put (c);
+         end loop;
+         New_Line;
+      end loop;
    end printlines;
-   
+
 --procedure half(given: natural) is
---begin 
+--begin
 --   Put_Line(given / 2 );
---end half;   
-   
+--end half;
+
 -- no need for declare here
-   upper : constant integer := 12;
+   upper : constant Integer := 12;
 begin
-    printlines(2, upper, '=');
-   -- do something 
-    printlines(3, 20, '-');
+   printlines (2, upper, '=');
+   -- do something
+   printlines (3, 20, '-');
 --    half (10) ;
 end demo;
