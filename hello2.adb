9f1f05a62f09a4493869c6644723f27f30fedf8a
Mr M J H <matroxmike@icloud.com>
2018-04-17 19:32:28 +1000

    tidy

--- hello2.adb
+++ hello2.adb
@@ -1,11 +1,10 @@
 with Ada.Text_IO;
-use Ada.Text_IO;
+use  Ada.Text_IO;
 
 --Print a message out to the screen
 -- date Saturday 13 April 2013
 -- https://gcc.gnu.org/onlinedocs/gnat_ugn/Running-a-Simple-Ada-Program.html
 -- https://stackoverflow.com/questions/12462461/is-it-worth-to-learn-ada-instead-of-another-languages-c-c
-
 
 procedure Hello2 is
 
