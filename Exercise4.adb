826886c4ba5b6eff0a9ce5a7310953c04535dc1b
Mike <matroxmike@icloud.com>
2018-08-07 16:35:42 +1000

    minor mod

--- Exercise4.adb
+++ Exercise4.adb
@@ -10,10 +10,13 @@ begin
     Put_Line("1. Single Family");
     Put_Line("2. Townhouse");
     Put_Line("3. Condominium");
-    Put_Line("You Choice? ");
+    Put_Line("Your Choice? ");
     Ada.Integer_Text_IO.Get(Item);
    
    if Item = 1 then
       Put_Line("House Type: Single Family");      
+   end if;
+   if Item = 2 then
+     Put_Line("House Type: Townhouse");      
    end if;
 end Exercise4;
