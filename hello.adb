5aaa3fc432ca3b239c092e30f754abf8fee046c0
Mike <matroxmike@icloud.com>
2019-10-07 16:05:03 +1100

    re-visit

--- hello.adb
+++ hello.adb
@@ -5,7 +5,7 @@ use Ada.Text_IO;
 -- date Saturday 13 April 2013
 -- https://gcc.gnu.org/onlinedocs/gnat_ugn/Running-a-Simple-Ada-Program.html
 -- https://stackoverflow.com/questions/12462461/is-it-worth-to-learn-ada-instead-of-another-languages-c-c
-
+-- revisited on 07.10.2019 15:10:39
 
 procedure Hello is
 
@@ -13,6 +13,7 @@ begin
    for i in 1..10 loop
       put('z');
      end loop;
+   Put_Line("");
    Put_Line("Hello Mike from Ada");
    Put_Line("second line");
 end hello;
