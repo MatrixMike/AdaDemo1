68d5597c88a0af178499a41bca2807fccdd2dc36
Mike <matrixmike@icloud.com>
2019-10-08 21:29:58 +1100

    working script and Ada compilation with hex file

---
+++ addpath3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#addpath3.sh
+#PATH=/media/mike/moreSpace/gnatARM/bin:$PATH
+#/media/mike/moreSpace/gnatARM/bin
+arm-eabi-objcopy  -O ihex    obj/main    out.hex
+cp out.hex /media/mike/MICROBIT/
