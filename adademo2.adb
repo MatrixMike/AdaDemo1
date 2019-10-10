78cd8240033d50f87469defa37ce005818cf8198
MatroxMike on Optiplex <matroxmike@icloud.com>
2019-10-10 19:35:17 +1100

    in process of repairing2

--- adademo2.adb
+++ adademo2.adb
@@ -43,21 +43,26 @@ end loop;
 --  a := a + 1;
 --  exit when a = 10;
 -- end loop;
-
+for i in 0..5 loop
 case i is
   when 0 => Ada.Text_IO.Put ("zero");
   when 1 => Ada.Text_IO.Put ("one");
   when 2 => Ada.Text_IO.Put ("two");
   -- case statements have to cover all possible cases:
   when others => Ada.Text_IO.Put ("none of the above");
+  Ada.Text_IO.Put (" _ ");
 end case;
-
-for aWeekday in Weekday'Range loop               -- loop over an enumeration
-   Put_Line ( Weekday'Image(aWeekday) );         -- output string representation of an enumeration
-   if aWeekday in Working_Day then               -- check of a subtype of an enumeration
-      Put_Line ( " to work for " &
-               Working_Hours'Image (Work_Load(aWeekday)) ); -- access into a lookup table
-   end if;
 end loop;
+
+--for aWeekday in Weekday'Range loop               -- loop over an enumeration
+--   Put_Line ( Weekday'Image(aWeekday) );         -- output string representation of an enumeration
+--   if aWeekday in Working_Day then               -- check of a subtype of an enumeration
+--     Put_Line ( " to work for " &
+--              Working_Hours'Image (Work_Load(aWeekday)) ); -- access into a lookup table
+--   end if;
+--
+--
+--
+--end loop;
 end adaDemo2;
 
